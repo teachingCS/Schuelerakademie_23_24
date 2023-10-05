6331121755f98567e3fdae1cf3c161a15bbbb98b
Stefan <hoermann@cpg-gilching.de>
2023-10-05 17:12:09 +0200

    Stand 05.10.2023

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Hier werden alle Projekte gespeichert, die im Rahmen des Kurses 2D & 3D Spielepr
 Verwandelt den Button in einen "Click-counter" Button.
 Bei jedem Klick auf den Button wird auf dem Button angezeigt, wie oft der Button geklickt wurde. Viel Spaß und Erfolg dabei!
 (Dieser Text steht auch als Kommentar im Skript "ratespiel.gd")
+
+## Programmierherausforderungen bis zum nächsten Treffen am 12.10:
+###Herausforderung 1:
+Beim Anklicken des RestartButtons soll das Spiel mit allen nötigen Werten wieder zurückgesetzt werden, so dass das Spiel nicht ständig neu gestartet werden muss.
+### Herausforderung 2:
+An geeigneter Stelle soll mittels eines passenden Nodes angezeigt werden, wie viele Rateversuche bereits gebraucht wurden.
+
+Viel Spaß und Erfolg!
--- Ratespiel/ratespiel.gd
+++ Ratespiel/ratespiel.gd
@@ -1,10 +1,19 @@
 extends Control
 
+# Attribut zur Hausaufgabe
+var counter : int = 0
+
+# Attribute für unser Ratespiel
+var computer_number : int
+var player_number : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	computer_number = randi_range(0, 200)
+	print(computer_number)
 	greet_player()
-	$Titel.text = "Ratespiel"
+	%Titel.text = "Rate eine Zahl zwischen 0 und 200"
+	
 func greet_player():
 	var name
 	name = "Godot"
@@ -12,9 +21,32 @@ func greet_player():
 
 
 func _on_button_pressed():
-	print("Der Button wurde gedrückt!")
+	# Teil der Hausaufgabe
+	counter = counter + 1
+	$Button.text = "Klicks: " + str(counter)
+#	print("Der Button wurde gedrückt!")
 
 
-# Hausaufgabe: Verwandelt den Button in einen Click-counter Button
-# Bei jedem Klick auf den Button wird auf dem Button angezeigt, wie oft
-# der Button geklickt wurde. Viel Spaß und Erfolg dabei! 
+
+func _on_check_button_pressed():
+	player_number = $VBoxContainer/SpinBox.value
+	if (player_number < computer_number):
+		%InfoLabel.text = "Die geratene Zahl ist zu klein."
+	elif (player_number > computer_number):
+		%InfoLabel.text = "Die geratene Zahl ist zu groß."
+	else:
+		%InfoLabel.text = "RICHTIG!!!"
+		restart_game()
+
+func restart_game():
+	%RestartButton.show()
+	%CheckButton.hide()
+
+# Programmierherausforderungen bis zum nächsten Treffen am 12.10:
+# Herausforderung 1:
+# Beim anklicken des RestartButtons soll das Spiel mit allen 
+# nötigen Werten wieder zurückgesetzt werden, so dass das Spiel
+# nicht ständig neu gestartet werden muss
+# Herausforderung 2:
+# An geeigneter Stelle soll mittels eines passenden Nodes angezeigt werden
+# wie viele Rateversuche bereits gebraucht wurden

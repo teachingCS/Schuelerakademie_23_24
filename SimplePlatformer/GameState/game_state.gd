62036d16614c45b4562f58a419dec7ac26946faa
Stefan <hoermann@cpg-gilching.de>
2023-12-21 10:39:12 +0100

    inkl Bonusvideos

--- SimplePlatformer/GameState/game_state.gd
+++ SimplePlatformer/GameState/game_state.gd
@@ -1,1 +1,13 @@
 extends Node
+
+@export var current_level := 0
+@export var levels : Array[Resource]
+
+func load_level():
+	var scene
+	# Alle Level sind gespielt:
+	if current_level == levels.size():
+		get_tree().quit()
+	else:
+		scene = levels[current_level]
+		get_tree().change_scene_to_packed(scene)
--- SimplePlatformer/Levels/level_02.gd
+++ SimplePlatformer/Levels/level_02.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 
+
 func _on_goal_body_entered(_body):
 	GameState.current_level += 1
 	GameState.load_level()
